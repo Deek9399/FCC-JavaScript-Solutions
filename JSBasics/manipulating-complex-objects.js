21618c109c0fb5debfb80b0e256a9c58f6122c06
Deeksha Tiwari <deeksha2021@gmail.com>
2020-08-19 21:50:58 +0530

    more files added as js

---
+++ JSBasics/manipulating-complex-objects.js
@@ -0,0 +1,21 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  ,
+
+ {
+   "artist": "Shashwat",
+   "title": "Tabla vadAK",
+   "release_year": 1999,
+   "formats": ["aa","bb","cc"]
+ } // Add a record here
+];
