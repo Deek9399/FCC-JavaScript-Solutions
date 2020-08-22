3a719d8a55b9f5b54bf181b9ba64f50ee73ca510
Deeksha Tiwari <deeksha2021@gmail.com>
2020-08-22 15:07:39 +0530

    RegEx completed

---
+++ RegularExpressions/check-for-mixed-grouping-of-characters.js
@@ -0,0 +1,5 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor).*Roosevelt/g; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+console.log(result)
