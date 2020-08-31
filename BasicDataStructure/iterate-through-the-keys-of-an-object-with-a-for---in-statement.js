8a76bed5b1708bc52d035ee7b177bb0f81f43242
Deeksha Tiwari <deeksha2021@gmail.com>
2020-08-31 15:12:45 +0530

    Basic data structures completed

---
+++ BasicDataStructure/iterate-through-the-keys-of-an-object-with-a-for---in-statement.js
@@ -0,0 +1,12 @@
+function countOnline(usersObj) {
+  // Only change code below this line
+ let result = 0;
+  for (let user in usersObj) {
+    if (usersObj[user].online === true) {
+      result++;
+    }
+  }
+  return result;
+  // Only change code above this line
+}
+//console.log(countOnline(users));
